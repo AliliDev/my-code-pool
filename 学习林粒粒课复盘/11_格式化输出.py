3b3c73f786dd0a71a29fd7eaa5ac7f72dfd8d117
阿狸不是兽 <157333370+AliliDev@users.noreply.github.com>
2026-04-10 17:44:09 +0800

    Add files via upload

---
+++ 学习林粒粒课复盘/11_格式化输出.py
@@ -0,0 +1,6 @@
+student_marks ={"小明":3.214,
+                "小花":1.14514,
+                "小李":2.456,
+                "小张":4.557}
+for name,marks in student_marks.items():
+    print("{0}你好你的绩点为:{1:.2f}".format(name,marks))
